b80305070756aad1bfdf9ace827b7b6f723e9dd0
u1137734 <mownetharan.aks@iqvia.com>
2024-04-05 00:09:00 +0530

    re structuring and adding projects

---
+++ Gemini - OCR/app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from ocr import ocr, llm
+import cv2
+import numpy as np
+
+st.set_page_config(layout = 'wide')
+uploaded_file = st.file_uploader("Upload your image file", type=['png', 'jpg', 'jpeg'])
+
+if uploaded_file is not None:
+
+    file_bytes = uploaded_file.getvalue()
+    np_arr = np.fromstring(file_bytes, np.uint8)
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    if img is not None:
+        st.image(img, caption='Processed Image')
+
+    col1, col2 = st.columns(2)
+    col1.write("OCR - pytesseract")
+    col2.write("LLM - Gemini")
+
+    # Initialize variables to store results
+    #if st.session_state.ocr_result is None:
+
+    #st.session_state.llm_result = None
+
+    ocr_b = col1.button("Read", key=1)
+    if ocr_b:
+        st.session_state.ocr_result = ocr(img)
+
+    llm_b = col2.button("Read", key=2)
+    if llm_b:
+        st.session_state.llm_result = llm(img)
+
+    if st.session_state.ocr_result:
+        col1.success(st.session_state.ocr_result)
+    if st.session_state.llm_result:
+        col2.warning(st.session_state.llm_result)
