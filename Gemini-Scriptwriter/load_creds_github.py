a1e507ada3f9e06593121c0f77d3455d6e01fa17
u1137734 <mownetharan.aks@iqvia.com>
2024-04-07 17:14:25 +0530

    plotwriter

--- Gemini-Scriptwriter/load_creds_github.py
+++ Gemini-Scriptwriter/load_creds_github.py
@@ -34,13 +34,13 @@ def load_creds():
                   "token_uri":token_uri,
                   "auth_provider_x509_cert_url":auth_provider_x509_cert_url,
                   "client_secret":client_secret,
-                  "redirect_uris":["https://plotwriter.streamlit.app"]}
+                  "redirect_uri":"https://plotwriter.streamlit.app"}
         }
         flow = InstalledAppFlow.from_client_config(
             client_config,
-            scopes=["https://www.googleapis.com/auth/generative-language.tuning"]
+            scope="https://www.googleapis.com/auth/generative-language.tuning"
         )
-        auth_url, _ = flow.authorization_url( redirect_uri = ["https://plotwriter.streamlit.app"])
+        auth_url, _ = flow.authorization_url(prompt='consent' )
         st.write("Please visit this URL to authorize access:", auth_url)
 
         # Ask the user to enter the authorization code obtained after authorizing access
