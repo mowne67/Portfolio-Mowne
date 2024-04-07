3d8215d8b965c2b4495d57d1eef30fb21baa0919
u1137734 <mownetharan.aks@iqvia.com>
2024-04-07 14:23:25 +0530

    plotwriter

--- Gemini-Scriptwriter/load_creds_github.py
+++ Gemini-Scriptwriter/load_creds_github.py
@@ -1,6 +1,8 @@
 import os
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
 
 SCOPES = ['https://www.googleapis.com/auth/generative-language.tuning']
 
