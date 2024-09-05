8cda1ba51c1e70389a6bcf90bc0568cb46803103
u1137734 <mownetharan.aks@iqvia.com>
2024-09-05 21:37:25 +0530

    Added Talk to excel to streamlit cloud

--- Talk_2_Excel/app.py
+++ Talk_2_Excel/app.py
@@ -9,21 +9,20 @@ st.info('by Mowne')
 st.logo("https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg",
         link="https://www.linkedin.com/in/mowne")
 uploaded_file = st.file_uploader("Upload your Excel/CSV file", type=['xlsx', 'csv'])
-if uploaded_file: df = pd.read_csv(uploaded_file)
-
-api_key = st.secrets['GOOGLE_API_KEY']
-genai.configure(api_key=api_key)
-
-llm = ChatGoogleGenerativeAI(model='gemini-1.5-flash')
-agent_executor = create_pandas_dataframe_agent(
-    llm,
-    df,
-    verbose=True,
-    allow_dangerous_code=True,
-    handle_parsing_errors=True,
-    max_iterations = 1000
-)
 if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    api_key = st.secrets['GOOGLE_API_KEY']
+    genai.configure(api_key=api_key)
+
+    llm = ChatGoogleGenerativeAI(model='gemini-1.5-flash')
+    agent_executor = create_pandas_dataframe_agent(
+        llm,
+        df,
+        verbose=True,
+        allow_dangerous_code=True,
+        handle_parsing_errors=True,
+        max_iterations = 1000
+    )
     st.dataframe(df)
     question = st.text_input("Ask away!")
     st.success(agent_executor.invoke(question)['output'])
